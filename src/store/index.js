236a017c36e15f68d8ef70bfc9fd4d77920cdeb6
joelmukanya <joel@lifechoices.co.za>
2022-08-04 11:38:45 +0200

    Picknpay GUI

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,30 @@
-import { createStore } from 'vuex'
-
+import { createStore } from 'vuex';
+import axios from 'axios';
+// import router from '@/router';
+// Picknpay url
+const picknpayUrl = "https://picknpayapi.herokuapp.com/";
 export default createStore({
   state: {
+    products: null
   },
   getters: {
+    getProducts: state => state.products
   },
   mutations: {
+    setProducts(state, values) {
+      state.products = values
+    }
   },
   actions: {
+    fetchProducts: async (content)=> {
+      let result = await axios.get(picknpayUrl);
+      let { data } = await result.results;
+      if(data) {
+        content.commit('setProducts', data);
+      }else {
+        console.log("There is no data");
+      }
+    }
   },
   modules: {
   }

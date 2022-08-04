23b834a6a2f4541996c1a3a1d04cce07d471f722
joelmukanya <joel@lifechoices.co.za>
2022-08-04 19:03:50 +0200

    To Be Continue...

--- src/store/index.js
+++ src/store/index.js
@@ -5,17 +5,25 @@ import axios from 'axios';
 const picknpayUrl = "https://picknpayapi.herokuapp.com/";
 export default createStore({
   state: {
+    users: null,
     products: null
   },
   getters: {
+    getUsers: state => state.users,
     getProducts: state => state.products
   },
   mutations: {
+    setUsers(state, values) {
+      state.users = values
+    },
     setProducts(state, values) {
       state.products = values
     }
   },
   actions: {
+    // fetchUsers: async (content) => {
+      
+    // },
     fetchProducts: async (content)=> {
       let res = await axios.get(picknpayUrl+"products");
       let { results }  = await res.data;
